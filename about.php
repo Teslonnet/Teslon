2d9fa6600e7a6570b04c789d86193263be74a5d4
Teslon <142211890+Teslonnet@users.noreply.github.com>
2023-08-17 19:10:23 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>TESLON - About</title>
+<meta charset="utf-8">
+<meta name="description" content="Teslon wants to help finance and develop renewable energy projects. This contributes to lower carbon emissions and a more sustainable future for the planet.">
+<meta name="keywords" content="Teslon about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="Teslon - About" />
+<meta property="og:description" content="Teslon is a project Designed to support green energy and the electric vehicle industry. It provides the user with the opportunity to finance projects related to the development of clean energy sources, which helps to accelerate the transition to cleaner technologies." />
+<meta property="og:image" content="https://teslon.tech/img/meta/mobile/Teslon1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://teslon.tech" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="Teslon" />
+
+<meta name="twitter:title" content="Teslon - About">
+<meta name="twitter:description" content="Teslon wants to help finance and develop renewable energy projects. This contributes to lower carbon emissions and a more sustainable future for the planet.">
+<meta name="twitter:image" content="https://teslon.tech/img/meta/mobile/Teslon1200.png">
+<meta name="twitter:site" content="@Teslonnet">
+<meta name="twitter:creator" content="@Teslonnet">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>About</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+				Learn about Teslon
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The Project</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The Tesla logo is a stylized image of the letter "T" in the form of an emblem. The letter "T" is made in a recognizable font and has arrows pointing up and down, which symbolizes movement and energy. The logo is usually presented in black on a white background, but can also be used in other color variations. It is one of the most recognizable and identifying elements of the Tesla brand.<br><br>
+
+									 Teslon is a cryptocurrency created in 2023 and named after Tesla. Teslon uses blockchain technology to provide secure and anonymous financial transactions. The Teslon logo is a stylized image of the letter "T", similar to the Tesla logo, but with the addition of additional cryptocurrency-related elements.
+<br><br>
+Teslon was created to stimulate the development of the electric vehicle industry and support Tesla. Teslon offers unique financing opportunities for projects related to electric vehicles and green energy. Teslon also wants to issue physical coins that can be used to buy goods and services from stores that accept cryptocurrencies.
+<br><br>
+One of the main characteristics of Teslon is its anonymity. Users can send and receive payments without revealing their personal information. This makes Teslon attractive to those who value the privacy and security of their financial transactions.
+<br><br>
+In addition, Teslon has low transaction fees and fast confirmation times. This makes it convenient to use in daily payments and transfers.
+<br><br>
+The total supply of Teslon is limited to 127 million coins, which guarantees stability and security. The cryptocurrency also wants to actively participate in improving and expanding the functionality of Teslon.
+<br><br>
+Overall, Teslon is an innovative cryptocurrency that combines blockchain technology with the support of the electric vehicle and green energy industries. Teslon offers secure, anonymous and convenient financial transactions and investments.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="600" height="600" viewBox="0,0,256,256"
+style="fill:#000000;">
+<g fill="#dc2f2f" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(2,2)"><path d="M64,23.77539c-15.625,0 -31.24961,2.97383 -46.09961,8.92383c-1.5,0.6 -2.30117,2.40039 -1.70117,3.90039c0.6,1.5 2.40039,2.30117 3.90039,1.70117c28.3,-11.3 59.50078,-11.3 87.80078,0c0.4,0.1 0.69961,0.19922 1.09961,0.19922c1.2,0 2.30078,-0.70039 2.80078,-1.90039c0.6,-1.6 -0.10117,-3.30039 -1.70117,-3.90039c-14.85,-5.95 -30.47461,-8.92383 -46.09961,-8.92383zM54.55664,38.18359c-0.18242,-0.01387 -0.36914,-0.00937 -0.55664,0.01563c-10.6,1 -20.90039,3.5 -30.90039,7.5c-0.8,0.3 -1.49922,1.00039 -1.69922,1.90039c-0.3,0.9 -0.20117,1.8 0.29883,2.5l5.30078,8.30078c0.6,0.9 1.5,1.40039 2.5,1.40039c0.6,0 1.09961,-0.2 1.59961,-0.5c1.4,-0.9 1.80039,-2.70156 0.90039,-4.10156l-3.30078,-5.19922c7.5,-2.7 15.2,-4.5 23,-5.5l9.30078,56.69922c0.2,1.5 1.5,2.5 3,2.5h0.5c1.3,-0.2 2.2,-1.19844 2.5,-2.39844l9.30078,-56.70117c7.8,0.9 15.5,2.8 23,5.5l-3.30078,5.20117c-0.9,1.4 -0.49961,3.29961 0.90039,4.09961c0.5,0.3 1.09961,0.5 1.59961,0.5c1,0 2,-0.50039 2.5,-1.40039l5.30078,-8.30078c0.5,-0.8 0.59883,-1.7 0.29883,-2.5c-0.2,-1 -0.89922,-1.7 -1.69922,-2c-9.9,-4 -20.30039,-6.5 -30.90039,-7.5c-1.6,-0.1 -2.89922,0.90039 -3.19922,2.40039v0.09961l-6.80078,41.5l-6.80078,-41.5v-0.09961c-0.2625,-1.3125 -1.36563,-2.31894 -2.64258,-2.41602z"></path></g></g>
+</svg>	
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+				
+                <div class='splitter-img-wrapper' style="background: url('/img/graphics/about/battery-tesla-svgrepo-com.svg') no-repeat center; background-size: cover; width: 550px; height: 550px">
+					</div>
+				
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The History (2023 — Present)</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Teslon was created in 2023 by a group of enthusiasts who were inspired by the success of Tesla and its contribution to the development of the electric car industry. They decided to create a cryptocurrency that would support this industry and help fund projects related to green energy.
+<br><br>
+
+Currently, Teslon continues to develop and attract new users. Teslon wants to become one of the most popular and environmentally friendly cryptocurrency.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Advantages of Teslon
+
+						</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 
+									Teslon wants to help finance and develop renewable energy projects. This contributes to lower carbon emissions and a more sustainable future for the planet.<br><br>
+
+Teslon provides users with the opportunity to invest in projects related to green energy and electric vehicles. Buying and long-term holding of Teslon can be profitable as the price of the coin can rise over time.
+<br><br>
+In general, Teslon is a cryptocurrency that not only brings financial benefits, but also supports environmental projects.
+									 
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+							
+
+
+
+				<div class='splitter-img-container splitter-con-right'>
+				<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/Tsloncoin.png') no-repeat center; background-size: cover; width: 550px; height: 550px">
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Team</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Teslon is a decentralized development ecosystem, which means there is no “official” team or formal hierarchy. This global developer community is an unlimited pre-ocracy that anyone can participate in. Individuals and teams choose tasks for themselves and carry them out. We Collectively Achieve Rough Consensus in the Teslcoin Improvement Proposal Process. <a href='https://twitter.com/Teslonnet?t=GW96tSvIKyWb3Vca8NxW-w&s=09' target="_blank">Twitter</a> </span>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="user-con-grouped">
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/dh.png') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/graphics/flags/unknown.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							JD </span>
+							<span class="user-con-role user-role-founder">
+							Project Founder </span>
+							<span class="user-con-role user-role-lead">
+							Lead Core Developer </span>
+						</div>
+						<div class='user-con-divider role-founder darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							Jacob Davis
+the father of Teslcoin, who founded the project on May 23, 2023. He is responsible for the development of Teslon. <a href='https://github.com/R' target="_blank">Twitter</a> </span>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/hykem.png') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/graphics/flags/unknown.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							LH </span>
+							<span class="user-con-role user-role-founder">
+							Project Founder </span>
+							<span class="user-con-role user-role-lead">
+							Lead Core Developer </span>
+						</div>
+						<div class='user-con-divider role-founder darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							Logan Harris
+Web developer and graphic designer. He is responsible for the overall branding and presentation of the project on the World Wide Web. <a href='http://www.emunewz.net/forum/showthread.php?tid=174735' target="_blank">Twitter</a> </span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>

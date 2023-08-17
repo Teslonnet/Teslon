e844f9b2e060d30eae8eeb337c7e9829143f14a2
Teslon <142211890+Teslonnet@users.noreply.github.com>
2023-08-17 19:10:43 +0300

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Teslon - FAQs</title>
+<meta charset="utf-8">
+<meta name="description" content="Teslon is a cryptocurrency that not only brings financial benefits, but also supports environmental projects and the development of the electric vehicle industry.">
+<meta name="keywords" content="Teslon faq">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="TESLON - FAQs" />
+<meta property="og:description" content="Teslon is a cryptocurrency that not only brings financial benefits, but also supports environmental projects and the development of the electric vehicle industry." />
+<meta property="og:image" content="https://teslon.tech/img/meta/mobile/Teslon1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://teslon.tech" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="TESLON" />
+
+<meta name="twitter:title" content="Teslon - FAQs">
+<meta name="twitter:description" content="Teslon is a cryptocurrency that not only brings financial benefits, but also supports environmental projects and the development of the electric vehicle industry.">
+<meta name="twitter:image" content="https://teslon.tech/img/meta/mobile/Teslon1200.png">
+<meta name="twitter:site" content="@Teslonnet">
+<meta name="twitter:creator" content="@Teslonnet">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>FAQs</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Frequently asked questions
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>1. What is Teslan? </h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Teslon is a project designed to support green energy and the electric vehicle industry. Provides the user with the opportunity to finance projects related to the development of clean energy sources, which helps to accelerate the transition to cleaner technologies. The project is also actively working to improve the functionality and security of Teslon to ensure reliability and security for users. Overall, Teslon is a useful tool to support the development of green energy and the production of electric vehicles.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>2. Who created Teslon?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Teslon was created by Jacob Davis and Logan Harris. to support clean energy projects: Teslon helps finance and develop renewable energy projects. This contributes to lower carbon emissions and a more sustainable future for the planet.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>3. How do Teslon work?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Teslon technology is no different from some other "real" cryptocurrencies, and its underlying mechanism is very similar to those of well-known altcoins such as Ethereum and Litecoin.<br><br>Teslon is a digital payment system that does not involve banks in verifying transactions. It is a peer-to-peer system that allows anyone, anywhere to send and receive payments. Cryptocurrency payments exist exclusively digitally in an online database describing specific transactions. They do not involve operations with physical money that has circulation and the possibility of exchange exchange in the real world. When transferring funds in cryptocurrency, transactions are recorded in a public ledger.<br><br>Teslon works in the same way as other cryptocurrencies, i.e. using the blockchain.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>4. What does Teslon want?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Helps finance and develop projects related to the use of renewable energy sources. This contributes to lower carbon emissions and a more sustainable future for the planet.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>5. What is the use of Teslon?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Provides users with the opportunity to invest in projects related to green energy and electric vehicles. Buying and holding Teslon long-term can be profitable as the price of the coin can rise over time.<br><br>Overall, Teslon is a cryptocurrency that not only brings financial benefits, but also supports environmental projects and the development of the electric vehicle industry.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>6. What is the technology behind Teslon?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Teslon is based on blockchain technology, similar to other cryptocurrencies such as Bitcoin and Litecoin. It uses the Proof-of-Work algorithm to secure the network and verify transactions.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>7. What is the value of Teslon?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Teslon is a cryptocurrency that was created to support and finance projects related to the development of electric vehicles and alternative energy sources. The goal of Teslon is to help fund research and development in electric vehicles, solar panels and other technologies that can improve environmental sustainability and reduce dependence on oil resources.
+						</p>
+						
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>8. How to safely store Teslon?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Teslon can be stored in cryptocurrency wallets, which can be online, on a computer, or on hardware devices. It is important to choose a reliable and secure wallet to protect your coins from being hacked or stolen. Metamask is a non-custodial (over-the-counter) crypto wallet that allows you to receive, store and send cryptocurrency assets. The Metamask wallet exists as an extension for the Google Chrome family of browsers and as a smartphone app. <a href="https://metamask.io/download/">Wallet official site MetaMask</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>9.How can I get a Teslon?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						A popular way to get a Teslon is through an exchange. To buy Teslon through PancakeSwap, you must already have Binance Smart Chain coins (BNB, BUSD and other BEP20) in one of the wallets, such as MetaMask or WalletConnect. You can buy them, for example, on the Binance crypto exchange. <br><br>Go to <a href="https://pancakeswap.finance/swap?outputCurrency=0x2F62b6F8EA2855f003158070FAb57EAc8D29Bb33">pancakeswap.finance</a> and click the Connect Wallet button in the top right corner. Then select one of the connection methods, we recommend using MetaMask or WalletConnect (available in the Trust Wallet mobile wallet):
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>10.How many Teslon coins?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						How many Teslon tokens were issued in total? The current volume of Teslon is 127 million.
+                        At the same time, everyone can become a little detective and with the help of such online platforms, in a few clicks, get information about previously completed transactions with a specific address, its current balance, amounts passed through it, or address lists for incoming (input) and outgoing (output) transactions. <a href="https://bscscan.com/address/0x2f62b6f8ea2855f003158070fab57eac8d29bb33">BscScan</a>.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>How can I join the Teslon community?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						There are many ways to get involved in the Teslon community! Most of the popular social media platforms have a thriving and welcoming Teslon community through which newcomers and old timers alike can connect, discuss and share their thoughts, memes, ideas and share the joy of all things Teslon. <a href="https://twitter.com/Teslonnet?t=GW96tSvIKyWb3Vca8NxW-w&s=09">Twitter</a> <a href="https://t.me/teslontech">Telegram</a> <a href="https://discord.gg/nTTbQhVY">Discord</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>I would like to create a patch, contribute, or maybe become a developer. Where to begin?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						This is great, contributions are welcome! Find something you want to implement or improve (such as broken links, bugs, missing features, roadmap goals, etc.), work on it, test your changes, and submit a pull request. If you have any questions, please contact us on our <a href="https://twitter.com/Teslonnet?t=GW96tSvIKyWb3Vca8NxW-w&s=09">Twitter</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Where can I submit issues, feedback and comments that I want to report?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p> Teslon is a decentralized development ecosystem, which means there is no “official” team or formal hierarchy. This global developer community is an unlimited interruption that anyone can participate in. Individuals and teams choose tasks for themselves and carry them out. We collectively reach a rough consensus in the process of proposals to improve Teslon. <a href="https://twitter.com/Teslonnet?t=GW96tSvIKyWb3Vca8NxW-w&s=09">Twitter</a></p>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include 'lib/module/inc-footer.php';?>
+</body>
+</html>

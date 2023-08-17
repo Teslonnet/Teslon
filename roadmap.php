d5ef069ae3eff4a719feaf710e5d1010cf32107a
Teslon <142211890+Teslonnet@users.noreply.github.com>
2023-08-17 19:10:58 +0300

    Add files via upload

---
+++ roadmap.php
@@ -0,0 +1,494 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>TESLON - Roadmap</title>
+<meta charset="utf-8">
+<meta name="description" content="Our roadmap is a non-exhaustive list of current goals that we aim to achieve through new developments and continuous integration.">
+<meta name="keywords" content="Teslon roadmap">
+<meta name="author" content="TESLON">
+<meta name="copyright" content="TESLON">
+<link rel="icon" type="image/png" href="/img/meta/favicon/teslon16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="/img/meta/favicon/teslon32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="/img/meta/favicon/teslon96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="/img/meta/favicon/teslon120.png" sizes="128x128" />
+<link rel="icon" type="image/png" href="/img/meta/favicon/teslon192.png" sizes="196x196" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/57.png" sizes="57x57" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/60.png" sizes="60x60" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/72.png" sizes="72x72" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/76.png" sizes="76x76" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/114.png" sizes="114x114" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/120.png" sizes="120x120" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/144.png" sizes="144x144" />
+<link rel="apple-touch-icon" href="/img/meta/mobile/152.png" sizes="152x152" />
+<link rel="manifest" href="/lib/js/manifest.json">
+<meta name="theme-color" content="#686de0">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><meta property="og:title" content="TESLON - Roadmap" />
+<meta property="og:description" content="Our roadmap is a non-exhaustive list of current goals that we aim to achieve through new developments and continuous integration." />
+<meta property="og:image" content="https://teslon.tech/img/meta/mobile/Teslon1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://teslon.tech" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="TESLON" />
+<meta name="twitter:title" content="TESLON - Roadmap">
+<meta name="twitter:description" content="Our roadmap is a non-exhaustive list of current goals that we aim to achieve through new developments and continuous integration.">
+<meta name="twitter:image" content="https://teslon.tech/img/meta/mobile/Teslon1200.png">
+<meta name="twitter:site" content="@Teslonnet">
+<meta name="twitter:creator" content="@Teslonnet">
+<meta name="twitter:card" content="summary_large_image">
+<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v4" />
+<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v4" />
+<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v4" />
+<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v4" />
+<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v4" />
+<script data-cfasync="false" src="/lib/js/jquery.js?v4"></script>
+<script data-cfasync="false" src="/lib/js/dark.js?v4"></script>
+<script data-cfasync="false" src="/lib/js/main.js?v4"></script>
+<script data-cfasync="false" src="/lib/js/jscookie.js?v4"></script>
+<script data-cfasync="false" src="/lib/js/particles.js?v4"></script>
+<script data-cfasync="false" src="/lib/js/particles-sel.js?v4"></script>
+<noscript>
+<style>
+/*
+General:
+- Elements that fade in are not visible by default
+*/
+.fade-up-onstart {
+	display:inherit;
+}
+.fade-in-onload {
+	display:inherit;
+}
+/*
+Index:
+- Center index contents
+*/
+.landing-con-container {
+	top:50%;
+	margin-top:-139px;
+}
+/*
+Quickstart:
+- Remove dimming
+- Do not block interaction outside the sidebar
+- Slightly reduce the width of the sidebar
+*/
+.toggle-navsidebar {
+	background:none;
+	width:auto;
+}
+.sidebar-con-anim {
+	width:300px;
+}
+</style>
+</noscript>
+</head>
+<body>
+
+
+
+<div class="window-con-theme">
+<div class="window-bg-theme">
+</div>
+<div class="theme-con-spinner">
+<div class="theme-ico-spinner">
+</div>
+</div>
+
+<div class="wavebar-con-container-theme">
+<div class="wavebar-con-wrap">
+<div class="wavebar-svg-object">
+</div>
+<div class="wavebar-svg-object">
+</div>
+</div>
+</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+<div class="settings-menu-ico-darkmode">
+</div>
+<div class="settings-menu-btn-button btn-darkmode">
+<span>Dark Theme - On/Off</span>
+</div>
+</div>
+<div class="settings-menu-con-divider">
+</div>
+<div class="settings-menu-con-wrapper toggle-transparency">
+<div class="settings-menu-ico-transparency">
+</div>
+<div class="settings-menu-btn-button btn-transparency">
+<span>Transparency - On</span>
+</div>
+</div>
+<div class="settings-menu-con-divider">
+</div>
+<div class="settings-menu-con-wrapper toggle-particles">
+<div class="settings-menu-ico-particles">
+</div>
+<div class="settings-menu-btn-button btn-particles">
+<span>Particles - On</span>
+</div>
+</div>
+<div class="settings-menu-con-divider">
+</div>
+<div class="settings-menu-con-wrapper toggle-pulsate">
+<div class="settings-menu-ico-pulsate">
+</div>
+<div class="settings-menu-btn-button btn-pulsate">
+<span>Pulsate - On</span>
+</div>
+</div>
+<div class="settings-menu-con-divider">
+</div>
+<div class="settings-menu-con-wrapper toggle-waves">
+<div class="settings-menu-ico-waves">
+</div>
+<div class="settings-menu-btn-button btn-waves">
+<span>Waves - On</span>
+</div>
+</div>
+<div class="settings-menu-con-divider">
+</div>
+<div class="settings-menu-con-wrapper toggle-theme settings-menu-lower" style="border-top: solid 1px rgba(0,0,0,.02);">
+<div class="settings-menu-ico-theme">
+</div>
+<div class="settings-menu-btn-button btn-theme">
+<span>Change Theme</span>
+</div>
+</div>
+</div>
+<div class="menu-btn-backtotop">
+</div>
+<div class="menu-con-container">
+<div class="menu-con-backdrop darkmode-menu">
+</div>
+<div class="menu-btn-settings toggle-settings">
+</div>
+<div class="menu-con-outer">
+<div class="menu-con-inner">
+<div class="menu-con-logo">
+<a href="/">
+<div class="menu-ico-logo">
+</div>
+</a>
+</div>
+<div class="mobile-menu-btn-open toggle-mobilemenu">
+</div>
+<div class="mobile-menu-btn-darkmode toggle-darkmode">
+</div>
+
+<a href="/about">
+<div class="menu-btn-select">
+<span>About</span>
+</div>
+</a>
+
+
+<a href="/roadmap">
+<div class="menu-btn-select">
+<span>Roadmap</span>
+</div>
+</a>
+<a href="/faq">
+<div class="menu-btn-select">
+<span>FAQs</span>
+</div>
+</a>
+<div class="menu-con-divider">
+</div>
+<a href="https://github.com/rpcs3" target="_blank">
+<div class="menu-btn-select">
+<span>GitHub</span>
+</div>
+</a>
+<a href="https://forums.rpcs3.net" target="_blank">
+<div class="menu-btn-select">
+<span>Forum</span>
+</div>
+</a>
+<a href="https://wiki.rpcs3.net">
+<div class="menu-btn-select">
+<span>Wiki</span>
+</div>
+</a>
+<a href="https://discord.me/RPCS3" target="_blank">
+<div class="menu-btn-select">
+<span>Discord</span>
+</div>
+</a>
+<a href="https://www.youtube.com/c/RPCS3_emu/videos" target="_blank">
+<div class="menu-btn-select">
+<span>YouTube</span>
+</div>
+</a>
+<div class="support-con-menu">
+<a href="https://www.patreon.com/Nekotekina" target="_blank">
+<div class="support-ico-menu">
+</div>
+</a>
+</div>
+</div>
+</div>
+</div>
+<div class="mobile-menu-con-container popup-mobilemenu" style="display:none;">
+<div class="menu-con-backdrop-2">
+</div>
+<div class="mobile-menu-con-scroll">
+<div class="mobile-menu-con-outter">
+<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+<div class="mobile-menu-tx1-group">
+<span>Main</span>
+</div>
+<div class="mobile-menu-con-divider">
+</div>
+<a href="/">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>Home</span>
+</div>
+</a>
+
+<a href="/about">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>About</span>
+</div>
+</a>
+
+
+<a href="/roadmap">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>Roadmap</span>
+</div>
+</a>
+<a href="/faq">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>FAQs</span>
+</div>
+</a>
+
+
+
+
+<div class="mobile-menu-tx1-group">
+<span>Social</span>
+</div>
+<div class="mobile-menu-con-divider">
+</div>
+<a href="https://github.com/rpcs3" target="_blank">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>GitHub</span>
+</div>
+</a>
+<a href="https://twitter.com/Teslonnet?t=GW96tSvIKyWb3Vca8NxW-w&s=09" target="_blank">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>Twitter</span>
+</div>
+</a>
+<a href="https://t.me/teslontech" target="_blank">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>Telegram</span>
+</div>
+</a>
+<a href="https://discord.gg/nTTbQhVY" target="_blank">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>Discord</span>
+</div>
+</a>
+
+<div class="mobile-menu-btn-spacer">
+</div>
+</div>
+</div>
+</div>
+</div><div class="page-con-content">
+<div class="banner-con-container darkmode-header">
+<div id="object-particles">
+</div>
+<div class="wavebar-con-container">
+<div class="wavebar-con-wrap">
+<div class="wavebar-svg-object">
+</div>
+<div class="wavebar-svg-object">
+</div>
+</div>
+</div>
+<div class="banner-con-backdrop darkmode-header">
+</div>
+<div class="banner-con-title fade-up-onstart">
+<div class="banner-tx1-title fade-up-onstart pulsate">
+<h1>Roadmap</h1>
+</div>
+<div class="banner-con-divider">
+</div>
+<div class="banner-tx2-title fade-up-onstart">
+<p>
+Goals that have been set for the project
+</p>
+</div>
+</div>
+</div>
+<div class="page-con-container">
+<div class="page-in-container">
+<div class="markdown darkmode-block">
+
+<p>The Teslon roadmap is a plan for developing and achieving the goals of the Teslon project. It defines the main milestones and tasks that Teslon intends to accomplish in the near future.</p>
+<h2><a id="user-content-short-term-goals" class="anchor" aria-hidden="true" href="#short-term-goals"><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path></svg></a>Short term goals</h2>
+<ul>
+<li>Team formation and concept development (Q1 2023): In this phase, the Teslon team will gather experts and develop a project strategy. A market analysis and a study of user needs will be carried out.</li>
+<li>Blockchain Platform Development (Q2-Q3 2023): Teslon will work on building its own blockchain platform for Teslon. This will ensure the security and transparency of transactions, as well as expand the capabilities of the project.</li>
+
+</ul>
+<h2><a id="user-content-medium-term-goals" class="anchor" aria-hidden="true" href="#medium-term-goals"><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path></svg></a>Medium term goals</h2>
+<ul>
+<li>Mainnet launch and coin issuance (Q4 2024): After the development of the blockchain platform is completed, Teslon will launch the Teslon mainnet and issue a coin. This will allow users to start using Teslon to conduct transactions and earn rewards.</li>
+<li>Ecosystem Development and Partnerships (2024): During this phase, Teslon will work to develop the Teslon ecosystem by attracting new users and partners. There will be marketing campaigns and partnership agreements to increase the use of Teslon in various industries.</li>
+
+
+</ul>
+<h2><a id="user-content-long-term-goals" class="anchor" aria-hidden="true" href="#long-term-goals"><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path></svg></a>Long term goals</h2>
+<ul>
+<li>Introducing new features and performance improvements (2025): Teslon will continue to develop and implement new features and improvements to improve Teslon performance and usability. Research and development will be carried out in the field of blockchain technologies and cryptocurrencies.</li>
+<li>Scaling and Global Presence (2025+): During this phase, Teslon will look to expand Teslon global footprint by partnering with international organizations and expanding its user base. Events and conferences will be held to draw attention to the project.</li>
+
+</ul>
+
+<h2><a id="user-content-for-developers" class="anchor" aria-hidden="true" href="#for-developers"><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path></svg></a>For developers</h2>
+<ul>
+<p>The Teslon Roadmap is a general plan for the development of the project, which may be subject to changes and additions depending on changing conditions and market needs.</p>
+
+</ul>
+</div>
+</div>
+</div>
+</div>
+<footer>
+<div class="footer-con-container">
+<div class="footer-con-overlay darkmode-header">
+<div class="wavebar-con-container">
+<div class="wavebar-con-wrap">
+<div class="wavebar-svg-object">
+</div>
+<div class="wavebar-svg-object">
+</div>
+</div>
+</div>
+<div class="footer-con-header">
+<div class="footer-con-outer">
+<div class="footer-con-inner">
+<div class="footer-ico-logo darkmode-footer-logo">
+</div>
+<table class="footer-table-out">
+<tr>
+<td>
+<div class="footer-table-in">
+<div class="footer-con-bound">
+<div class="footer-tx1-bound">
+<span>Main</span>
+</div>
+
+<div class="footer-tx2-bound">
+<a href="/about">
+<span>About</span>
+</a>
+</div>
+
+
+<div class="footer-tx2-bound">
+<a href="/roadmap">
+<span>Roadmap</span>
+</a>
+</div>
+<div class="footer-tx2-bound">
+<a href="https://github.com/RPCS3/rpcs3" target="_blank">
+<span>Repository</span>
+</a>
+</div>
+</div>
+</div>
+</td>
+<td>
+<div class="footer-table-in">
+<div class="footer-con-bound">
+<div class="footer-tx1-bound">
+<span>Social</span>
+</div>
+<div class="footer-tx2-bound">
+<a href="https://twitter.com/Teslonnet?t=GW96tSvIKyWb3Vca8NxW-w&s=09" target="_blank">
+<span>Twitter</span>
+</a>
+</div>
+<div class="footer-tx2-bound">
+<a href="https://t.me/teslontech" target="_blank">
+<span>Telegram</span>
+</a>
+</div>
+<div class="footer-tx2-bound">
+<a href="https://discord.gg/nTTbQhVY" target="_blank">
+<span>Discord</span>
+</a>
+</div>
+
+
+</div>
+</div>
+</td>
+</tr>
+</table>
+</div>
+</div>
+</div>
+<div class="footer-con-credits">
+<div class="footer-con-outer">
+<div class="footer-con-inner">
+<div class="footer-con-seprator">
+</div>
+<div class="footer-con-foot">
+<div class="footer-con-side">
+<div class="footer-tx1-founder">
+<div class="footer-ico-developer" style="background: url('/img/icons/footer/dh.png') center center / cover no-repeat;">
+</div>
+<div class="footer-ico-developer" style="background: url('/img/icons/footer/hykem.png') center center / cover no-repeat;">
+</div>
+<a href="/about">
+<span>Founded by JD and LH</span>
+</a>
+</div>
+</div>
+<div class="footer-con-side">
+<div class="footer-tx1-webmaster">
+<div class="footer-ico-developer" style="background: url('/img/users/tesla-5512.png') center / cover no-repeat;">
+</div>
+
+
+<span>Website by TESLON © 2023</span>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer><script defer src="https://static.cloudflareinsights.com/beacon.min.js/v8b253dfea2ab4077af8c6f58422dfbfd1689876627854" integrity="sha512-bjgnUKX4azu3dLTVtie9u6TKqgx29RBwfj3QXYt5EKfWM/9hPSAI/4qcV5NACjwAo8UtTeWefx6Zq5PHcMm7Tg==" data-cf-beacon='{"rayId":"7f594c6d9c239d9f","token":"2d98f3b9a0394355ab27c66293c3546e","version":"2023.7.0","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
